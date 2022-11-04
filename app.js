7549751c43978b2c2878644e962e1fe812e7db67
Kamil <prospero.apps@gmail.com>
2022-11-04 14:03:40 +0100

    Cors moved up in code

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ const helmet = require('helmet')
 
 const app = express();
 
+app.use(cors())
 app.use(helmet())
 
 // Database connection
@@ -58,7 +59,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(compression())
-app.use(cors())
+
 
 app.use(passport.initialize())
 
